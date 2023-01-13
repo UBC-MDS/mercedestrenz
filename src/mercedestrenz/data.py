e570acc62b9f41aa81c915d8a770e40b36af3491
kellywujy <kellywu817@outlook.com>
2023-01-12 22:19:34 -0800

    doc: parameter description added

--- src/mercedestrenz/data.py
+++ src/mercedestrenz/data.py
@@ -22,3 +22,25 @@ def load_sample_mercedes_listings()-> pd.DataFrame():
 
 # Author: Kelly Wu
 # Date: 2023-01-12
+def listing_search(max_price=30000, region = "any", model = "any", sort_feature = "odometer", ascending = True):
+    """
+    Return the top listings that are within the budget specified by the user, sorted by price from lowest to highest.
+    The results are filtered by optional input, region and model.
+    The results are also sorted by ascending price and another the specified feature in the sort_feature parameter. 
+    By default the sort_feature is lower mileage value, but user has the flexibility to choose another numeric attribute.
+
+    Return an empty pandas dataframe when there is no listing that satisfies the criteria and print a warning message.
+    
+    Parameters
+    ----------
+    max_price : float
+        The maximum of the budget range.
+    region : string
+        The region of the listing. The default is to include listings from any region.
+    model : string
+        The model of the car that the user is interested in. The default is to include listings of any model.
+    sort_feature : string
+        The numeric variable that the user is interested in using to sort the result. The default value is to sort by odometer value.
+    ascending : bool
+        Boolean value that indicate whether the sort is ascending. The default value is True.
+    """
