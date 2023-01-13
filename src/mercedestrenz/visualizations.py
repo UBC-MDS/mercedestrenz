3531dca1c045546d63e03a88ef23b859fd5b7d1f
MO ZHAO <morrisz@student.ubc.ca>
2023-01-12 19:52:59 -0800

    introduction of the three parameters

--- src/mercedestrenz/visualizations.py
+++ src/mercedestrenz/visualizations.py
@@ -3,4 +3,13 @@ def plot_benz(model, price, market_df):
     Plot a density plot of a specific Mercedes-Benz model to see where 
     the current vehicle's price falls within the distribution of prices 
     for that model in the market.
+    
+    Parameters
+    ----------
+    model : str
+        The model of the vehicle intersted.
+    price : float
+        The price of the vehicle intersted.
+    market_df : pandas.DataFrame
+        Dataframe containing information on used Mercedes-Benz in the market.
     """
