15de0c75b38fc09ac2d847fc52626b8092c317b6
MO ZHAO <morrisz@student.ubc.ca>
2023-01-12 19:52:16 -0800

    adding descibtion for the function

--- src/mercedestrenz/visualizations.py
+++ src/mercedestrenz/visualizations.py
@@ -1,1 +1,6 @@
 def plot_benz(model, price, market_df):
+    """
+    Plot a density plot of a specific Mercedes-Benz model to see where 
+    the current vehicle's price falls within the distribution of prices 
+    for that model in the market.
+    """
