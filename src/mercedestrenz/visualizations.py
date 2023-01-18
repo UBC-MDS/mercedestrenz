a0d6a8bec43ba731413b53a30428bb0de5507b6f
MO ZHAO <morrisz@student.ubc.ca>
2023-01-17 20:18:19 -0800

    plot the density plot

--- src/mercedestrenz/visualizations.py
+++ src/mercedestrenz/visualizations.py
@@ -42,7 +42,7 @@ def plot_mercedes_price(model, price, market_df):
     # filter the data set for the specific model
     # model_df = listing_search(market_df, model = model)
     
-        # Add current price to the data set
+    # Add current price to the data set
     market_df['x'] = price
     
     # Target column is price
@@ -50,4 +50,38 @@ def plot_mercedes_price(model, price, market_df):
     
     # Caculating the median of the market
     median = np.percentile(market_df['price'], 50)
+    
+        # different message will be send base on price
+    if price > median:
+        message = f"The input {target_col} = {price} is larger than the median of the market"
+    else:
+        message = f"The input {target_col} = {price} is smaller than the median of the market"
+    
+    # Define plot title
+    plot_title = alt.TitleParams(
+    f"Density Plot of Mercedes-Benz {model}",
+    subtitle = message)
+
+    # Create density plot for the specific model
+    density_plot = alt.Chart(
+        market_df, title = plot_title
+    ).transform_density(
+        target_col, as_=[target_col, 'density']
+    ).mark_area(
+        opacity=0.9
+    ).encode(
+        alt.X(f'{target_col}:Q', title="Price", 
+              axis=alt.Axis(format='s', tickSize=0)),
+        alt.Y('density:Q', title='Density', 
+              axis=alt.Axis(labels=False, tickSize=0)),
+        tooltip=f'{target_col}:Q'
+    )
+    
+    # Create a line indicates where current car price is.
+    line = alt.Chart(market_df).mark_rule(color='red', size=2).encode(x='x')
+    
+    # Combine the density plot and line
+    final_plot = density_plot + line
+    
+    return final_plot
     
