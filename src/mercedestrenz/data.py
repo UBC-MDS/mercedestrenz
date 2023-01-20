959e24949b8428f22c3bb0961903e1a092de6d3d
kellywujy <kellywu817@outlook.com>
2023-01-19 16:26:11 -0800

    unit tests added

--- src/mercedestrenz/data.py
+++ src/mercedestrenz/data.py
@@ -57,7 +57,43 @@ def listing_search(data, budget=[0, np.Inf], model = "any", sort_feature = "odom
     >>> result.head()
     #to be completed with exmaple output
     """
-    pass
 
+    # ======= Unit tests ==========
+    # check input data is a pd.dataframe
+    if type(data) != pd.DataFrame:
+        raise Exception("The input dataset is not of Pandas DataFrame format")
+
+    # check budget is either a flort/int or a list of float/int
+    if len(budget) == 1:
+        # check if float or int
+        if type(budget) not in [float, int]:
+            raise Exception("The maximum budget should be a float or a integer")
+    elif len(budget) == 2:
+        for elem in budget:
+            if type(elem) not in [float, int]:
+                raise Exception("The budget range boundaries should be floats or integers")
+    else:
+        raise Exception("Please specify your maximum budget using a number, or a range of budge using a list of numeric values.")
+
+    # model= parameter input should be a string
+    if type(model) != str:
+            raise Exception("The model parameter should have a string as the input")
+    
+    # The string should correspond to a specific car model in the model column
+    if (data['model'].eq(model)).any() == False:
+        raise Exception("The specified car model does not exist in the dataframe provided")
+
+    # sort_feature should be a string
+    if type(sort_feature) != str:
+        raise Exception("The input value for sort_feature parameter should be a string")
+    
+    # sort_feature should indicate a numeric column in the provided data frame
+    numeric_cols = data.select_dtypes('number').columns.to_list()
+    if type(sort_feature) not in numeric_cols:
+        raise Exception("The specified sort_feature should be a numeric column in the provided dataframe.")
+
+    # ascending is either true or false
+    if type(ascending) != bool:
+        raise Exception("Please specify True or False in the ascending parameter")
 
    
