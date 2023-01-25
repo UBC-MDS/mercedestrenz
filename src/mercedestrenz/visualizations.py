8c50217ce925f6974e408ca35bfafbf8251dda63
kellywujy <kellywu817@outlook.com>
2023-01-24 16:39:09 -0800

    bug fixed

--- src/mercedestrenz/visualizations.py
+++ src/mercedestrenz/visualizations.py
@@ -36,6 +36,7 @@ def plot_mercedes_price(model, price, market_df, model_col = 'model', price_col 
     >>> plot_mercedes_price(model='S-Class', price=80000, market_df=market_df))
     >>> plot_mercedes_price(model='C-Class', price=10000, market_df=used_car_df))
     """
+
     # Test if inputs have correct type
     if type(model) != str:
         raise Exception('The first input should be a string contain the specific model')
@@ -55,15 +56,18 @@ def plot_mercedes_price(model, price, market_df, model_col = 'model', price_col 
     # Target column is price
     target_col = price_col
     
+
     # filter the data set for the specific model
     # market_df = listing_search(market_df, model = model)
     market_df = market_df[market_df[model_col] == model]
 
     # Add current price to the data set
-    market_df['x'] = price
+    # market_df['x'] = price
+    df2 = market_df.assign(x=price)
+
     
     # Caculating the median of the market
-    median = np.percentile(market_df['price'], 50)
+    median = np.percentile(market_df[target_col], 50)
     
     # different message will be send base on price
     if price > median:
@@ -85,14 +89,14 @@ def plot_mercedes_price(model, price, market_df, model_col = 'model', price_col 
         opacity=0.9
     ).encode(
         alt.X(f'{target_col}:Q', title="Price", 
-              axis=alt.Axis(format='s', tickSize=0)),
+              axis=alt.Axis( tickSize=0)),
         alt.Y('density:Q', title='Density', 
               axis=alt.Axis(labels=False, tickSize=0)),
         tooltip=f'{target_col}:Q'
     )
-    
+
     # Create a line indicates where current car price is.
-    line = alt.Chart(market_df).mark_rule(color='red', size=2).encode(x='x')
+    line = alt.Chart(df2).mark_rule(color='red', size=2).encode(x='x')
     
     # Combine the density plot and line
     final_plot = density_plot + line
