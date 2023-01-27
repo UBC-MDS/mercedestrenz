294bb365623338f244f06c9e1f141c09f6b46002
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-01-26 18:26:37 -0800

    bug: auto version update not working in GH action

--- src/mercedestrenz/__init__.py
+++ src/mercedestrenz/__init__.py
@@ -1,3 +1,4 @@
 # read version from installed package
 from importlib.metadata import version
-__version__ = version("mercedestrenz")
+# __version__ = version("mercedestrenz")
+__version__ = "0.1.0"
