cdd057327abcfe9e1d9b45d9801f7080cc4dc932
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-01-26 19:01:20 -0800

    bug: reset CICD setup to original structure
    Reset commands to not use Daniel's records but Tiffany's original
    commands from packaging a python.

--- src/mercedestrenz/__init__.py
+++ src/mercedestrenz/__init__.py
@@ -1,4 +1,4 @@
 # read version from installed package
 from importlib.metadata import version
-# __version__ = version("mercedestrenz")
-__version__ = "0.3.0"
+
+__version__ = version("mercedestrenz")
